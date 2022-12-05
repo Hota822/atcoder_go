392a7442f8343e97ec5ae561bc0de25b30376ad3
hotaka takayama <h_takayama@zisouz.co.jp>
2022-12-05 19:02:46 +0900

    20221205

---
+++ education_dp/3rd/frog-1.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	// "strings"
+	// "math"
+	// "reflect"
+	// "sort"
+)
+
+const (
+    max_bufSize = 1_000_000_000 // default: 65536
+	initial_buf = 10000
+	max_int32 = 2147483647
+	// prime_number = 1000_000_007
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func run() interface{} {
+	n := readInt()
+	// s := read()
+
+	sli := make([]int, n)
+	// for i:=0; i < n; i++ {
+    sli = readSli(n)
+    dp := make([]int, n)
+    dp[0] = 0
+    dp[1] = Abs(sli[0] - sli[1])
+    for i:=2; i<n; i++ {
+        i1 := dp[i -1] + Abs(sli[i] - sli[i -1])
+        i2 := dp[i -2] + Abs(sli[i] - sli[i -2])
+        dp[i] = Min(i1, i2)
+        p(dp[i])
+    }
+	// }
+
+	ans := dp[n -1]
+	return ans
+}
+
+
+// ========================read
+// func read() string {
+// 	sc.Scan()
+//     return sc.Text()
+// }
+
+// func readSli(n int) []string {
+func readSli(n int) []int {
+	// sli := make([]string, n)
+	sli := make([]int, n)
+	for i:=0; i < n; i++ {
+		// sli[i] = read()
+		sli[i] = readInt()
+	}
+    return sli
+}
+
+func readInt() int {
+    sc.Scan()
+    ret, _ := strconv.Atoi(sc.Text())
+    return ret
+}
+
+
+//=======================main========================
+func main() {
+	buf := make([]byte, initial_buf)
+	sc.Buffer(buf, max_bufSize)
+	sc.Split(bufio.ScanWords)
+	result := run()
+	print(result)
+}
+
+func print(ans interface{}) {
+	if v, ok := ans.(bool); ok {
+		if v {
+			fmt.Println("Yes")
+		} else {
+			fmt.Println("No")
+		}
+		return
+	}
+	if sli, ok := ans.([]int); ok {
+		for _, v := range sli {
+			fmt.Println(v)
+		}
+		return
+	}
+	if sli, ok := ans.([]int64); ok {
+		for _, v := range sli {
+			fmt.Println(v)
+		}
+		return
+	}
+	if sli, ok := ans.([]string); ok {
+		for _, v := range sli {
+			fmt.Println(v)
+		}
+		return
+	}
+	fmt.Println(ans)
+}
+
+// =============================math
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Abs(x int) int {
+	if x > 0 {
+		return x
+	}
+	return - x
+}
+
+func p(arg ...interface{}) {
+	_, _, l, _ := runtime.Caller(1)
+	s := strconv.Itoa(l)
+	fmt.Println("dumped at line: " + s + ", value: ")
+	for _, v := range arg {
+		fmt.Println(v)
+	}
+}
